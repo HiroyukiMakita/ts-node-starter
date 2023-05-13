e55bfdcd183b55edade2eac77aa3ad6d21cb9f42
HiroyukiMakita <hiroyukimakita.pc@gmail.com>
2023-05-13 13:59:41 +0900

    add: .env はちゃんと作り始めるとほぼほぼ入れるので dotenv を追加しとく

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 function hello() {
-    console.log('Hello TypeScript in ts-node!');
+    const envValue = process.env.ENV_VALUE;
+    if ((envValue ?? '') === '') {
+        throw new Error('Error: reading .env file\'s value failed.');
+    }
+    console.log('Hello TypeScript in ts-node!', `\nThis is ${envValue}`);
 }
 hello();
